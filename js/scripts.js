f08c4195f006d8489bf032c175b3ac35578acb83
epcruz <eduardaffpaulo@gmail.com>
2022-03-09 10:25:42 +0100

    fixing errors (ESLint)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-function newItem(){
+ /* eslint-env jquery */
+
+ // eslint-disable-next-line no-unused-vars
+function newItem(){ 
     //1. Adding a new item to the list of items: 
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
 
     if (inputValue === '') {
-        alert("You must write something!")
+        alert('You must write something!')
     } else {
         let list = $('#list');
         list.append(li);
@@ -13,22 +16,22 @@ function newItem(){
 
     //2. Crossing an item out:
     function crossOut() {
-        li.toggleClass("strike");
+        li.toggleClass('strike');
     }
 
-    li.on("dblclick",function crossOut(){
-        li.toggleClass("strike"); //why do we have to repeat here what crossOut function does?
+    li.on('dblclick',function crossOut(){
+        li.toggleClass('strike');
     });
 
     //3. Adding a delete button
     let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode("X"));
+    crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
     crossOutButton.on('click', deleteListItem);
 
     function deleteListItem(){
-        li.addClass("delete")
+        li.addClass('delete')
     }
 
     $('#list').sortable();
